5d6253ae6a70d86f98aef27fec0a6a537df33794
Geetha Yedida <yedida.geetha@gmail.com>
2016-01-29 14:48:13 -0700

    changes

--- SingletonImplementation.java
+++ SingletonImplementation.java
@@ -1,13 +1,31 @@
 package com.first.Jan92016.JavaTutorial;
 
+import java.util.Scanner;
+
 /**
  * Created by Geetha Yedida on 1/23/2016.
  */
 public class SingletonImplementation {
+
     public static void main(String[] args) {
+
+        EnumExample firstobj = EnumExample.getObject();
+        EnumExample secondobj = EnumExample.getObject();
+
         SingletonClassExample obj1 =  SingletonClassExample.getSingletonObject();
-        obj1.askUserChoice();
         SingletonClassExample obj2 = SingletonClassExample.getSingletonObject();
-        obj2.askUserChoice();
+
+        int choice = obj1.askUserChoice();
+
+        if (choice == 1){
+            firstobj.printAvailableOptions();
+            secondobj.askUserChoice();
+            firstobj.evaluateChoice();
+            firstobj.exit();
+        }
+        else if(choice == 2){
+            obj2.contacts();
+            obj1.exit();
+        }
     }
 }
